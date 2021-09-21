65641582d4b4a39697a1ee6c04561f68dae0921c
GenericGames <44574740+GenericGames@users.noreply.github.com>
2021-09-21 15:02:46 -0700

    added and finished

--- CodeAcademy/Python 3/Python Tests.py
+++ CodeAcademy/Python 3/Python Tests.py
@@ -1,1 +1,14 @@
-(5 * 2) - 1 == 8 + 1
+x = 20
+y = 20
+
+# Write the first if statement here:
+
+if x == y:
+    print("These numbers are the same")
+
+credits = 120
+
+# Write the second if statement here:
+
+if credits >= 120:
+    print("You have enough credits to graduate!")
