15be444899ad3149c2c3d5453c73313849e153e6
GenericGames <44574740+GenericGames@users.noreply.github.com>
2021-09-22 12:11:52 -0700

    Added and finished

--- CodeAcademy/Python 3/Python Tests.py
+++ CodeAcademy/Python 3/Python Tests.py
@@ -1,49 +1,16 @@
-weight = 4.8
-cost_ground = 0
-cost_ground_premium = 125.00
-cost_drone = 0
-cheapest_shipping = ""
+# Fortune Cookie Program 🥠
 
-## Ground Shipping
+import random
 
-if weight <= 2.0:
-    cost_ground = weight * 1.5 + 20.00
-elif weight > 2.0 and weight <= 6.0:
-    cost_ground = weight * 3.0 + 20.00
-elif weight > 6.0 and weight <= 10.0:
-    cost_ground = weight * 4.0 + 20.00
-elif weight > 10.0:
-    cost_ground = weight * 4.75 + 20.00
-else:
-    print("error")
+fortune = random.randint(0, 4)
 
-print("Ground shipping cost: $" + str(cost_ground))
-
-# Ground shipping premium
-
-print("Ground shipping premium cost: $" + str(cost_ground_premium))
-
-# Drone Shipping
-
-if weight <= 2.0:
-    cost_drone = weight * 4.5
-elif weight > 2.0 and weight <= 6.0:
-    cost_drone = weight * 9.0
-elif weight > 6.0 and weight <= 10.0:
-    cost_drone = weight * 12.0
-elif weight > 10.0:
-    cost_drone = weight * 14.25
-else:
-    print("error")
-
-print("Drone shipping cost: $" + str(cost_drone))
-
-# Cheapest shipping
-
-if cost_ground_premium < cost_ground and cost_ground_premium < cost_drone:
-    cheapest_shipping = "Ground premium"
-elif cost_ground < cost_drone:
-    cheapest_shipping = "Ground"
-else:
-    cheapest_shipping = "Drone"
-print(cheapest_shipping + " is the cheapest shipping method.")
+if fortune == 0:
+    print("May you one day be carbon neutral")
+elif fortune == 1:
+    print("You have rice in your teeth")
+elif fortune == 2:
+    print("No snowflake feels responsible for an avalanche")
+elif fortune == 3:
+    print("You can only connect the dots looking backwards")
+elif fortune == 4:
+    print("The fortune you seek is in another cookie")
