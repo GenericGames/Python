0a80a07a2705cdc5cebe0185e8108a8ef8e3e674
GenericGames <44574740+GenericGames@users.noreply.github.com>
2021-09-22 11:18:10 -0700

    Added and finished

--- CodeAcademy/Python 3/Python Tests.py
+++ CodeAcademy/Python 3/Python Tests.py
@@ -1,14 +1,49 @@
-x = 20
-y = 20
+weight = 4.8
+cost_ground = 0
+cost_ground_premium = 125.00
+cost_drone = 0
+cheapest_shipping = ""
 
-# Write the first if statement here:
+## Ground Shipping
 
-if x == y:
-    print("These numbers are the same")
+if weight <= 2.0:
+    cost_ground = weight * 1.5 + 20.00
+elif weight > 2.0 and weight <= 6.0:
+    cost_ground = weight * 3.0 + 20.00
+elif weight > 6.0 and weight <= 10.0:
+    cost_ground = weight * 4.0 + 20.00
+elif weight > 10.0:
+    cost_ground = weight * 4.75 + 20.00
+else:
+    print("error")
 
-credits = 120
+print("Ground shipping cost: $" + str(cost_ground))
 
-# Write the second if statement here:
+# Ground shipping premium
 
-if credits >= 120:
-    print("You have enough credits to graduate!")
+print("Ground shipping premium cost: $" + str(cost_ground_premium))
+
+# Drone Shipping
+
+if weight <= 2.0:
+    cost_drone = weight * 4.5
+elif weight > 2.0 and weight <= 6.0:
+    cost_drone = weight * 9.0
+elif weight > 6.0 and weight <= 10.0:
+    cost_drone = weight * 12.0
+elif weight > 10.0:
+    cost_drone = weight * 14.25
+else:
+    print("error")
+
+print("Drone shipping cost: $" + str(cost_drone))
+
+# Cheapest shipping
+
+if cost_ground_premium < cost_ground and cost_ground_premium < cost_drone:
+    cheapest_shipping = "Ground premium"
+elif cost_ground < cost_drone:
+    cheapest_shipping = "Ground"
+else:
+    cheapest_shipping = "Drone"
+print(cheapest_shipping + " is the cheapest shipping method.")
